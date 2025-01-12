2e66126ad15873455d161c8b71bf3502309f800b
mississppi <msspstudio@gmail.com>
2025-01-12 15:35:22 +0900

    setup template

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-# VPCの作成
+# ----------------------
+# VPC Configuration
+# ----------------------
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -8,7 +10,9 @@ resource "aws_vpc" "main" {
   }
 }
 
-# パブリックサブネットの作成
+# ----------------------
+# Subnet Configuration
+# ----------------------
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -19,7 +23,6 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-# プライベートサブネットの作成
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
@@ -27,4 +30,87 @@ resource "aws_subnet" "private_subnet" {
   tags = {
     Name = "private-subnet"
   }
+}
+
+# ----------------------
+# Route Table Configuration
+# ----------------------
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+# ----------------------
+# IGW Configuration
+# ----------------------
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# ----------------------
+# Route for Public Subnet via Internet Gateway
+# ----------------------
+
+resource "aws_route" "public_to_igw" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# ----------------------
+# NAT Gateway Configuration
+# ----------------------
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  vpc = true
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+# ----------------------
+# Route for Private Subnet via NAT Gateway
+# ----------------------
+
+resource "aws_route" "private_to_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+# ----------------------
+# Associate Private Route Table with Private Subnet
+# ----------------------
+
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private.id
 }
